aeb1cd67ea7dfd3ff77612cc00ad5ee9bb7759aa
Andrei Berceanu <andreicberceanu@gmail.com>
2023-08-23 17:34:05 +0300

    Update Karolina install instructions

--- Tools/machines/karolina-it4i/install_dependencies.sh
+++ Tools/machines/karolina-it4i/install_dependencies.sh
@@ -7,8 +7,8 @@ set -eu -o pipefail
 
 # Check: ######################################################################
 #
-#   Was karolina_synchrad.profile sourced and configured correctly?
-if [ -z ${proj-} ]; then echo "WARNING: The 'proj' variable is not yet set in your karolina_synchrad.profile file! Please edit its line 2 to continue!"; exit 1; fi
+#   Was karolina_fbpic.profile sourced and configured correctly?
+if [ -z ${proj-} ]; then echo "WARNING: The 'proj' variable is not yet set in your karolina_fbpic.profile file! Please edit its line 2 to continue!"; exit 1; fi
 
 
 # Remove old dependencies #####################################################
@@ -18,7 +18,7 @@ rm -rf ${SW_DIR}
 mkdir -p ${SW_DIR}
 
 # remove common user mistakes in python, located in .local instead of a venv
-python3 -m pip uninstall -qq -y synchrad
+python3 -m pip uninstall -qq -y fbpic
 python3 -m pip uninstall -qqq -y mpi4py 2>/dev/null || true
 
 
@@ -27,9 +27,9 @@ python3 -m pip uninstall -qqq -y mpi4py 2>/dev/null || true
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade virtualenv
 python3 -m pip cache purge
-rm -rf ${SW_DIR}/venvs/synchrad
-python3 -m venv ${SW_DIR}/venvs/synchrad
-source ${SW_DIR}/venvs/synchrad/bin/activate
+rm -rf ${SW_DIR}/venvs/fbpic
+python3 -m venv ${SW_DIR}/venvs/fbpic
+source ${SW_DIR}/venvs/fbpic/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade wheel
 python3 -m pip install --upgrade black 
@@ -40,16 +40,12 @@ python3 -m pip install --upgrade matplotlib
 python3 -m pip install --upgrade openpmd-api
 python3 -m pip install --upgrade openpmd-viewer
 python3 -m pip install --upgrade mpi4py --no-cache-dir --no-build-isolation --no-binary mpi4py
-python3 -m pip install --upgrade numba
+# TODO: remove commented lines
+#MPICC="cc -shared -target-accel=nvidia80" python3 -m pip install --upgrade --force --no-cache-dir --no-build-isolation --no-binary=mpi4py mpi4py
+#python3 -m pip install --upgrade llvmlite tbb
 python3 -m pip install --upgrade mako
 python3 -m pip install --upgrade pyopencl
-
-#TODO
-    python3 -m pip install openpmd-viewer
-    python3 -m pip install pyvista
-    python3 -m pip install fbpic
-    python3 -m pip install fbpic[picmi]
-    python3 -m pip install --upgrade numba llvmlite tbb --user
-    python3 -m pip install cupy-cuda115
-    MPICC="cc -shared -target-accel=nvidia80" python3 -m pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
-    python3 -m pip install pyfftw
+python3 -m pip install --upgrade numba
+python3 -m pip install --upgrade python-dateutil
+python3 -m pip install --upgrade h5py
+python3 -m pip install --upgrade cupy-cuda117
